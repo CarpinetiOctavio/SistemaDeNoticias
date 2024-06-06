8c886eabb4418e4bd8814c4a7e0a7b41459bcde5
CarpinetiOctavio <carpinetio@gmail.com>
2024-06-06 13:34:28 -0300

    mod

--- Articulo.cpp
+++ Articulo.cpp
@@ -3,3 +3,55 @@
 //
 
 #include "Articulo.h"
+
+void Articulo::setTitulo(std::string _titulo)
+{
+    titulo = _titulo;
+}
+
+std::string Articulo::getTitulo()
+{
+    return titulo;
+}
+
+void Articulo::setDetalle(std::string _detalle)
+{
+    detalle = _detalle;
+}
+
+std::string Articulo::getDetalle()
+{
+    return detalle;
+}
+
+void Articulo::setDia(int _dia)
+{
+    dia = _dia;
+}
+
+int Articulo::getDia()
+{
+    return dia;
+}
+
+void Articulo::setMes(int _mes)
+{
+    mes = _mes;
+}
+
+int Articulo::getMes()
+{
+    return mes;
+}
+
+void Articulo::setAnio(int _anio)
+{
+    anio = _anio;
+}
+
+int Articulo::getAnio()
+{
+    return anio;
+}
+
+Articulo::Articulo(std::string _titulo, std::string _detalle, int _dia, int _mes, int _anio) {};
--- Articulo.h
+++ Articulo.h
@@ -4,10 +4,30 @@
 
 #ifndef SISTEMADENOTICIAS_ARTICULO_H
 #define SISTEMADENOTICIAS_ARTICULO_H
-
+#include "iostream"
 
 class Articulo {
+private:
+    std::string titulo;
+    std::string detalle;
+    int dia;
+    int mes;
+    int anio;
 
+public:
+    void setTitulo (std::string _titulo);
+    void setDetalle (std::string _detalle);
+    void setDia (int _dia);
+    void setMes (int _mes);
+    void setAnio (int _anio);
+
+    std::string getTitulo();
+    std::string getDetalle ();
+    int getDia ();
+    int getMes ();
+    int getAnio ();
+
+    Articulo(std::string _titulo, std::string _detalle, int _dia, int _mes, int _anio);
 };
 
 
